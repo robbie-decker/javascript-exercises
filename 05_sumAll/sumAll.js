acef3fceb6e55be7178a4cc8fce48cfa3b8e8989
Robbie Decker <robbd56@gmail.com>
2023-05-09 11:40:17 -0400

    Solve problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end){
+    // If not a number or negative, return ERROR
+    if(typeof start != 'number' || typeof end != 'number' || start < 0 || end < 0){
+        return "ERROR";
+    }    
+    
+    if (start > end){
+        // swap variables
+        start = start + end;
+        end = start - end;
+        start = start - end;
+    }
+    // Factorial
+    let sum = 0;
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
